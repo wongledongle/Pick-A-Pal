34fde35d913a516167004bf8572744aa426a80db
Arthur <artbowong@gmail.com>
2024-06-23 13:33:49 -0700

    initcommit

---
+++ Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Arthur Wong on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
